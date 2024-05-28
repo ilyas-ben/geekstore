301c235e916026ea14ab74699e6895444cef76c8
ilyas <ilyasben313@gmail.com>
2024-05-28 22:06:31 +0100

    handling command form (not finished)

--- src/main/java/com/proj/enumm/City.java
+++ src/main/java/com/proj/enumm/City.java
@@ -1,4 +1,21 @@
 package com.proj.enumm;
 
 public enum City {
+    NULL(0),
+    CASABLANCA(1),
+    RABAT(2),
+    FES(3),
+    MARRAKECH(4),
+    TANGER(5);
+
+    private final int index;
+
+    City(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
 }
+
